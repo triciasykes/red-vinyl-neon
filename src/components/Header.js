518a720b4bf0bc1b2b33edabd999eba84ecd3ccd
triciasykes <tlsykes98@gmail.com>
2024-02-05 14:30:30 -0800

    src/

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
-import logo from "../assets/red-vinyl-45-logo-transparent-sm.png"
 
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
   return (
     <header className="text-white  z-10">
-      <section className="max-w-4xl mx-auto p-4 flex justify-center items-center">
+      <section className="max-w-4xl mx-auto p-4 justify-center items-center origin-top animate-open-menu md:hidden">
         <button
-          id="mobile-open-button"
-          className="text-3xl sm:hidden focus:outline-none"
+          id="hamburger-button"
+          className="text-3xl md:hidden cursor-pointer"
+          onClick={() => setIsNavOpen((prev) => !prev)}
         >
           &#9776;
         </button>
-        <nav className="hidden sm:block space-x-8 text-xl" aria-label="main">
+        <nav className="hidden md:block space-x-8 text-xl" aria-label="main">
           <Link to="/" className="link hover:opacity-30">
             Home
           </Link>
@@ -25,6 +28,44 @@ const Header = () => {
             Shows
           </Link>
           <Link to="/contact" className="link hover:opacity-30">
+            Contact Us
+          </Link>
+        </nav>
+      </section>
+      <section
+        id="mobile-menu"
+        className="absolute top-0 bg-black w-full text-5xl flex flex-col justify-content-center"
+      >
+        <button className="text-8xl self-end px-6">&times;</button>
+        <nav
+          className="flex flex-col min-h-screen items-center py-8"
+          aria-label="mobile"
+        >
+          <Link to="/" className="w-full text-center py-6 hover:opacity-90">
+            Home
+          </Link>
+          <Link
+            to="/songs"
+            className="w-full text-center py-6 hover:opacity-90"
+          >
+            Song List
+          </Link>
+          <Link
+            to="/videos"
+            className="w-full text-center py-6 hover:opacity-90"
+          >
+            Video
+          </Link>
+          <Link
+            to="/shows"
+            className="w-full text-center py-6 hover:opacity-90"
+          >
+            Shows
+          </Link>
+          <Link
+            to="/contact"
+            className="w-full text-center py-6 hover:opacity-90"
+          >
             Contact Us
           </Link>
         </nav>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,19 @@ module.exports = {
       serif: ["Tilt Neon"],
     },
     extend: {
+      screens: {
+        widescreen: { raw: "(min-aspect-ratio: 3/2)" },
+        tallscreen: { raw: "(min-aspect-ratio: 13/20)" },
+      },
+      keyframes: {
+        "open-menu": {
+          "0%": { transform: "scaleY(0)" },
+          "80%": { transform: "scaleY(1.2)" },
+          "100%": { transform: "scaleY(1)" },
+        },
+      },
       animation: {
-        "slow-spin": "spin 10s linear infinite",
+        "open-menu": "open-menu 0.5s ease-in-out forwards",
       },
     },
   },
